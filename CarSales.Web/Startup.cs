002bf15ed7162a36f3d11b1fd43568c226116ac3
Anh Le <anh.le@beteasy.com.au>
2020-06-20 17:29:22 +1000

    feat: Adding logging and swagger libraries

--- CarSales.Web/Startup.cs
+++ CarSales.Web/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Microsoft.OpenApi.Models;
 
 namespace CarSales.Web
 {
@@ -19,11 +21,46 @@ namespace CarSales.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            // Register Serilog
+            var logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            services.AddLogging(loggingBuilder => loggingBuilder.AddSerilog(logger));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CarSales Vehicle Web API",
+                    Description = "A simple Web API for CarSales coding assignment",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Anh Tuan Le",
+                        Email = "anhlt1983@gmail.com"
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CarSales Vehicle Web API v1");
+                // Server Swagger UI at the app's root
+                c.RoutePrefix = string.Empty;
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
